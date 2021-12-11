33487c32727a63831736d4fcf3a01d465756cf86
Tonia <tonia.saba@gmail.com>
2021-12-11 12:39:54 -0500

    able to calculate circle

---
+++ src/Circle.java
@@ -0,0 +1,37 @@
+
+// circle extends Shape
+
+public class Circle extends Shape {
+	
+	// radius attributes 
+	double radius;
+	double pi = Math.PI;
+	double circle_parameter;
+	double circle_area;
+	
+	// parameterized constructor needed initialize the data
+	
+   public Circle(double radius) {
+        this.radius = radius;
+    }
+   
+   //	"toString" method that prints out the area and parameter
+   
+   public String toString() {
+		return  "Parameter: " + circle_parameter + " Area: " + circle_area;
+   	
+   }
+   
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return circle_area = pi * Math.pow(radius, 2); 
+	}
+
+	@Override
+	public double parameter() {
+		// TODO Auto-generated method stub
+		return circle_parameter = 2 * pi * radius;
+	}
+
+}
